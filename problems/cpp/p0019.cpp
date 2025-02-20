82b98fa64b9a53a058393e9adf59364b632034c0
Richard <gdi_richard@yahoo.com>
2025-02-20 02:24:13 -0800

    Solved problem 19

--- problems/cpp/p0019.cpp
+++ problems/cpp/p0019.cpp
@@ -11,36 +11,24 @@
  class Solution {
     public:
         ListNode* removeNthFromEnd(ListNode* head, int n) {
-            
             ListNode* location = head;
-            ListNode* tempLocation;
-    
-            int count = 1;
-    
-            while ((location->next)!= nullptr ){
-                location = location->next;
-                count++;
-            }
-    
-            location = head;
-    
-            if ( count < 2 ){
-                head = nullptr;
-            }
-            else
+            ListNode* tempLocation = head;
+        
+            for ( int i = 0; i < n; i++ )
             {
-                for ( int ndx = 0; ndx < count - n; ndx++)
-                {
-                    location = location->next;
-                }
-    
-                tempLocation = location->next;
-                location->next = (location->next)->next;
-    
-                if ( tempLocation != nullptr )
-                    delete tempLocation;
+                location = location->next;
             }
     
+            if ( !location )
+                return head->next;
+    
+            while ( location->next ){
+                location = location->next;
+                tempLocation = tempLocation->next;
+            }
+    
+            tempLocation->next = (tempLocation->next)->next;
+       
             return head;
     
         }
