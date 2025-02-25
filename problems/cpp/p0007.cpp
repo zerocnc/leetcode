a174ea9310a61efc5dd7d28e47ec10a8dbf21ad5
Richard <gdi_richard@yahoo.com>
2025-02-24 22:43:42 -0800

    Added another solution for problem p0007.cpp

--- problems/cpp/p0007.cpp
+++ problems/cpp/p0007.cpp
@@ -52,3 +52,21 @@ class Solution01 {
             return rev;
         }
     };
+
+    class Solution02{
+        int reverse(int x) {
+            long sum = 0;
+        
+            while (x != 0) {
+                int rem = x % 10;
+                sum = sum * 10 + rem;
+                x = x / 10;
+            }
+            if (sum > INT_MAX || sum < INT_MIN) {
+                return 0;
+            }
+        
+            return (int)sum;
+        }
+    }
+
