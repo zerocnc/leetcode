16614b72c59f73089b8496f9ced914f54bcf1772
Richard <gdi_richard@yahoo.com>
2025-02-28 02:44:01 -0800

    Started problem p0023.cpp

---
+++ problems/cpp/p0023.cpp
@@ -0,0 +1,57 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+ class Solution {
+    public:
+        ListNode* mergeKLists(vector<ListNode*>& lists) {
+            
+            ListNode* start;
+            ListNode* temp1;
+    
+            if ( lists.size() > 0 ){
+                while ( lists.size() > 1 ){
+                    vector<ListNode*> temp;
+    
+                    for ( size_t i = 0; i < lists.size(); i += 2 ){
+                        ListNode* l1 = lists[i];
+                        ListNode* l2 = i + 1 < lists.size() ? lists[i+1] : nullptr;
+                        temp.push_back( mergeLists(l1, l2));
+                    }
+                    lists = move(temp);
+                }
+    
+    
+            }
+    
+            return lists[0];
+    
+        }
+    
+    private:
+        ListNode* mergeLists(ListNode* l1, ListNode* l2){
+            ListNode dummy;
+            ListNode* node = &dummy;
+    
+            while (l1 && l2){
+                if (l1->val > l2->val) {
+                    node->next = l2;
+                    l2 = l2->next;
+                } else {
+                    node->next = l1;
+                    l1 = l1->next;
+                }
+                node = node->next;
+    
+            }
+    
+            node->next = l1 ? l1 : l2;
+            return dummy.next;
+        }
+    };
