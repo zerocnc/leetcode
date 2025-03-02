5656a6a4f68c6135f201218cf770fef41102f939
Richard <gdi_richard@yahoo.com>
2025-03-02 00:02:59 -0800

    Modify p00024.cpp

--- problems/cpp/p0024.cpp
+++ problems/cpp/p0024.cpp
@@ -11,8 +11,33 @@
  class Solution {
     public:
         ListNode* swapPairs(ListNode* head) {
+
+            long long counter = 1;
+            bool swapCounter = false;
+            ListNode* pastNode = new ListNode;
+            ListNode* currentNode = new ListNode;
+            ListNode* swap = new ListNode;
+            ListNode* temp = new ListNode;
+
             if (head != nullptr ){
-    
+                currentNode = head;
+                
+                while ( currentNode != nullptr ){
+                    currentNode = currentNode->next;
+
+                    // swap
+                    // a->b->c
+                    // b->a->c
+
+                    // p1->p2->p3
+                    // l1 & l2
+
+                    // p1/l1 -> p2/l2 ->p3
+
+
+                
+                }
+
             }
     
             return head;
