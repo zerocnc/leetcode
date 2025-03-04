2fa44fbf8652f638ac44b626b47bf2f9227cf1fb
Richard <gdi_richard@yahoo.com>
2025-03-04 00:05:42 -0800

    started on p0009.cpp

--- problems/cpp/p0009.cpp
+++ problems/cpp/p0009.cpp
@@ -1,6 +1,51 @@
 class Solution {
     public:
         bool isPalindrome(int x) {
+    
+            // Need data structures stack & queue
+            std::queue<int> myQ;
+            std::stack<int> myS;
+    
+            bool flag = false;
+            bool mF = false;
             
+            if ( x >= 0 ){
+    
+                if ( x == 0 )
+                {
+                    flag = true;
+                }
+                
+                while ( x > 0 ){
+                    int temp = 0;
+                    temp = x % 10;
+                    x = x / 10;
+                    myQ.push(temp);
+                    myS.push(temp);
+    
+                }
+    
+                while ( myQ.empty() && myS.empty() ){
+                    int i;
+                    int j;
+    
+                    i = myQ.front();
+                    myQ.pop();
+    
+                    j = myS.top();
+                    myS.pop();
+    
+                    std::cout << i << ' ' << j << std::endl;
+    
+                    if ( i != j ){
+                        flag = false;
+                    }
+    
+                }
+    
+            }
+    
+            return flag;        
+    
         }
     };
