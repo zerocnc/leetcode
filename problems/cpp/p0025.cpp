3d67bc49b81eb05ebf31c8425e76914f7379f875
Richard <gdi_richard@yahoo.com>
2025-03-05 22:37:18 -0800

    Started p0025.cpp

---
+++ problems/cpp/p0025.cpp
@@ -0,0 +1,23 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+ class Solution {
+    public:
+        ListNode* reverseKGroup(ListNode* head, int k) {
+            
+            int x = 0;
+            ListNode temp(0,nullptr);
+            std::stack<ListNode> tempS;
+    
+    
+    
+            return head;
+        }
+    };
