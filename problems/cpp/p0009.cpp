73e9c407cb0a0ccc9bb0ea1efce690d57c8e5902
Richard <gdi_richard@yahoo.com>
2025-03-10 23:15:27 -0700

    Added another solution to problem 9

--- problems/cpp/p0009.cpp
+++ problems/cpp/p0009.cpp
@@ -49,3 +49,22 @@ class Solution {
     
         }
     };
+
+    class Solution_01 {
+        public:
+            bool isPalindrome(int x) {
+                if (x < 0) {
+                    return false;
+                }
+        
+                long reverse = 0;
+                int xcopy = x;
+        
+                while (x > 0) {
+                    reverse = (reverse * 10) + (x % 10);
+                    x /= 10;
+                }
+        
+                return reverse == xcopy;        
+            }
+        };
