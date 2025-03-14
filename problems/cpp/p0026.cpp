febf67941084a961200f915180033b1d0a018d3d
Richard <gdi_richard@yahoo.com>
2025-03-13 22:19:45 -0700

    Completed p0026.cpp

--- problems/cpp/p0026.cpp
+++ problems/cpp/p0026.cpp
@@ -1,6 +1,27 @@
 class Solution {
     public:
         int removeDuplicates(vector<int>& nums) {
-            
+            int lastInt = -101;
+            size_t count = 0;
+            size_t nums_size = nums.size();
+            std::vector<int> temp;
+    
+            for ( auto num : nums ){
+                if (lastInt != num ){
+                    temp.push_back(num);
+                    lastInt = num;
+                    count++;
+                }
+            }
+    
+            for ( size_t ndx = 0; ndx < count; ++ndx ){
+                nums[ndx] = temp[ndx];
+            }
+    
+            for (size_t ndx = count; ndx < nums_size; ndx++ ){
+                nums[ndx] = 0;
+            }
+    
+            return count;
         }
     };
