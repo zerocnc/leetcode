968adebc88e5bce64db3d638f1c22d1d3c1b6cb5
Richard <gdi_richard@yahoo.com>
2025-03-17 21:37:12 -0700

    Finished proble 27

--- problems/cpp/p0027.cpp
+++ problems/cpp/p0027.cpp
@@ -1,6 +1,25 @@
 class Solution {
     public:
         int removeElement(vector<int>& nums, int val) {
-            
+            size_t ndx = 0;
+            size_t count = 0;
+    
+            vector<int> temp;
+    
+            for (auto num : nums ){
+                if ( num != val )
+                {
+                    temp.push_back(num);
+                    count++;
+                }
+                    
+            }
+    
+            for ( size_t ndx = 0; ndx < count; ++ndx){
+                nums[ndx] = temp[ndx];
+            }
+    
+            return count;
+    
         }
     };
