987a4ebd097ffa4476c9b62e0dfc0b2c3ebd0448
Richard <gdi_richard@yahoo.com>
2025-03-18 23:58:36 -0700

    Halfway though p2460.cpp

--- problems/cpp/p2460.cpp
+++ problems/cpp/p2460.cpp
@@ -1,6 +1,29 @@
 class Solution {
     public:
         vector<int> applyOperations(vector<int>& nums) {
-            
+            vector<int> myTemp;
+            size_t count = 0;
+            size_t point_1 = 0;
+    
+            for ( size_t ndx = 0; ndx < nums.size() - 1; ndx++ ){
+                if ( nums[ndx] == nums[ ndx + 1 ]){
+                    nums[ndx] *= 2;
+                    nums[ndx+1] = 0;
+                    count++;
+                }
+            }
+    
+            for ( size_t ndx = 0; ndx < nums.size(); ndx++ ){
+                if ( nums[ndx] != 0 ){
+                    nums[point_1++] = nums[ndx];
+                }
+            }
+    
+            for ( size_t ndx = nums.size() - 1; ndx > 0; --ndx){
+                nums[ndx] = 0;
+            }
+    
+            return nums;
+    
         }
     };
