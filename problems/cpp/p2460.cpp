77fc0eea9fa0b9b436a4a6aa4f7a45d51ddbf2e7
Richard <gdi_richard@yahoo.com>
2025-03-19 21:57:30 -0700

    Sovled p2460 with brute force.

--- problems/cpp/p2460.cpp
+++ problems/cpp/p2460.cpp
@@ -1,32 +1,28 @@
 class Solution {
     public:
         vector<int> applyOperations(vector<int>& nums) {
-            vector<int> myTemp;
-            size_t count = 0;
-            size_t count2 = 0;
-            size_t point_1 = 0;
+            queue<int> myQueue;
+            queue<int> myQue2;
+            vector<int> tempVec;
     
             for ( size_t ndx = 0; ndx < nums.size() - 1; ndx++ ){
                 if ( nums[ndx] == nums[ ndx + 1 ]){
                     nums[ndx] *= 2;
                     nums[ndx+1] = 0;
-                    count++;
                 }
             }
     
-            for ( auto num : nums ){
-                if ( num != 0){
-                    myTemp.push_back(num);
-                    count2++;
+            for ( size_t i = 0; i < nums.size() - 1; ++i ){
+                for ( size_t j = i + 1; j < nums.size(); ++j ){
+                    if ( nums[i] == 0 ){
+                        int temp = nums[i];
+                        nums[i] = nums[j];
+                        nums[j] = temp;
+                    }
                 }
             }
     
-            if (count == 0)
-                count;
-    
-            myTemp.insert( myTemp.end(), count2, 0);
-    
-            return myTemp;
+            return nums;
     
         }
     };
