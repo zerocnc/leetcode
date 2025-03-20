e27bbeaa37a184a3fe134affff1b32c935f5399d
Richard <gdi_richard@yahoo.com>
2025-03-19 23:07:40 -0700

    Created optimal solution.

--- problems/cpp/p2460.cpp
+++ problems/cpp/p2460.cpp
@@ -23,3 +23,34 @@ class Solution {
     
         }
     };
+
+
+    class Solution_02 {
+        public:
+            vector<int> applyOperations(vector<int>& nums) {
+                int count = 0;
+                size_t point_1 = 0;
+        
+                for ( size_t ndx = 0; ndx < nums.size() - 1; ndx++ ){
+                    if ( nums[ndx] == nums[ ndx + 1 ]){
+                        nums[ndx] *= 2;
+                        nums[ndx+1] = 0;
+                        count++;
+                    }
+                }
+        
+                for ( size_t ndx = 0; ndx < nums.size(); ++ndx ){
+                    if ( nums[ndx] != 0){
+                        nums[point_1++] = nums[ndx];
+                    }
+                }
+        
+                while ( point_1 < nums.size() ){
+                    nums[point_1++] = 0;
+                }
+        
+        
+                return nums;
+        
+            }
+        };
