3d6a387b985f8a83588d0b45169c5df9b6c9ba31
Richard <gdi_richard@yahoo.com>
2025-03-20 23:57:12 -0700

    Going though p0028.cpp

--- problems/cpp/p0028.cpp
+++ problems/cpp/p0028.cpp
@@ -3,7 +3,7 @@ class Solution {
     
         bool compare(string segment, string word ){
     
-    
+            return true;
         }
     
         int strStr(string haystack, string needle) {
@@ -16,7 +16,16 @@ class Solution {
                 // Locate first letter
                 for ( size_t ndx = 0; ndx + needle.size() < haystack.size(); ndx++ ){
     
-                    if ( haystack[ndx] ==  )
+                    if ( haystack[ndx] == needle[0] ){
+                        // we compare the whole word.
+                        size_t needleSize = needle.size();
+    
+                        for ( size_t ndx2 = needleSize - 1; ndx2 >= 0; --ndx2 ){
+                            if ( haystack[ndx] != needle[ndx2]  )
+                                break;
+                        }
+    
+                    }
     
                 }
                 
