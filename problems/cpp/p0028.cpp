c7de4f8aaca575fb0e24047c5113df42d2bb55b2
Richard <gdi_richard@yahoo.com>
2025-03-21 00:04:35 -0700

    Finished problem p00028

--- problems/cpp/p0028.cpp
+++ problems/cpp/p0028.cpp
@@ -1,28 +1,27 @@
 class Solution {
     public:
     
-        bool compare(string segment, string word ){
-    
-            return true;
-        }
-    
         int strStr(string haystack, string needle) {
     
             size_t point_1 = -1;
             size_t sizeOfWord = needle.size();
     
-            if ( needle.size() < haystack.size() ){
+            if ( needle.size() <= haystack.size() ){
     
                 // Locate first letter
-                for ( size_t ndx = 0; ndx + needle.size() < haystack.size(); ndx++ ){
+                for ( size_t ndx = 0; ndx + needle.size() <= haystack.size(); ndx++ ){
     
                     if ( haystack[ndx] == needle[0] ){
                         // we compare the whole word.
-                        size_t needleSize = needle.size();
     
-                        for ( size_t ndx2 = needleSize - 1; ndx2 >= 0; --ndx2 ){
-                            if ( haystack[ndx] != needle[ndx2]  )
+                        for ( size_t ndx2 = needle.size() - 1; ndx2 >= 0; --ndx2 ){
+                            if ( haystack[ndx+ndx2] != needle[ndx2]  )
                                 break;
+                            else if ( haystack[ndx] == needle[ndx2] && ndx2 == 0  )
+                            {
+                                return ndx;
+                            }
+                                
                         }
     
                     }
