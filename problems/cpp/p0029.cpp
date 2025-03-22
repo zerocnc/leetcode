d949c38520824c06803330605372b0ae594737bb
Richard <gdi_richard@yahoo.com>
2025-03-21 23:11:42 -0700

    Problem p0029.cpp is solved.

--- problems/cpp/p0029.cpp
+++ problems/cpp/p0029.cpp
@@ -1,6 +1,25 @@
 class Solution {
     public:
         int divide(int dividend, int divisor) {
-            
+            if (dividend == INT_MIN && divisor == -1)  // Handle overflow case
+                return INT_MAX;
+    
+            bool negative = (dividend < 0) ^ (divisor < 0); // XOR to determine sign
+            long long dvd = abs((long long)dividend);  // Use long long to avoid overflow
+            long long dvs = abs((long long)divisor);
+    
+            int quotient = 0;
+            while (dvd >= dvs) {
+                long long temp = dvs, multiple = 1;
+                while (dvd >= (temp << 1)) { // Double divisor until it exceeds dividend
+                    temp <<= 1;
+                    multiple <<= 1;
+                }
+                dvd -= temp;
+                quotient += multiple;
+            }
+    
+            return negative ? -quotient : quotient;
         }
     };
+    
