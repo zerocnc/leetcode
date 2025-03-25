bcb8d06965c98d6cf8576c343f20f1b00a614a8d
Richard <gdi_richard@yahoo.com>
2025-03-25 00:25:33 -0700

    Did my best. Will continue tommorrow.

--- solutions/021-040/p0030.cpp
+++ solutions/021-040/p0030.cpp
@@ -1,27 +1,38 @@
 class Solution {
     public:
         bool compareString(string& s, vector<string> words){
-            int index = -1 ;
-            
+            int indexTemp = -1 ;
     
-            return index ? -1 ;
+            for ( size_t ndx = 0; ndx < )
+    
+            index = indexTemp;
+            return ( index > 0 ) ? true : false ;
         }
     
         vector<int> findSubstring(string s, vector<string>& words) {
             vector<string> temp = words;
             vector<int> indiciesFound = {};
             size_t wordLength = 0;
+            size_t tempIndex = -1;
     
             for ( auto word : words ){
                 wordLength += word.size();
+                wordLength -= 1;
                 temp.push_back(word);
             }
     
     
             for ( size_t ndx = 0; ndx < s.size() - wordLength() - 1; ++ndx ){
-                if ( s[ndx] == tempWord[0]  ){
-                    
+                for (size_t i = 0; ndx < s.size() - wordLength() - 1; ++i ){
+                    if ( s[ndx] == tempWord[0]  ){
+                        if ( compareString( s.substr( ndx, ndx + wordLength))){
+                            indiciesFound.push_back(ndx);
+                        }
+                    }
                 }
+    
             }
+    
+            return indiciesFound;
         }
     };
